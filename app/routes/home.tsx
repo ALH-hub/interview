fa9958d5a9243cbdebc2597afde2c3bc49de1b9e
Alhadji Oumate <alhadjioum0@gmail.com>
2026-05-19 10:17:49 +0100

    Refactor: Welcome component by removing unused logo imports and simplifying the structure

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,10 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
+import { Welcome } from '../welcome/welcome';
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: 'Job Interview' },
+    { name: 'interview', content: 'Give your best interview questions' },
   ];
 }
 
